74e330f73c13abb67b24717ab585de092ac038f6
honbin002-design <honbin002@gmail.com>
2026-08-30 20:09:20 +0800

    feat: add PWA offline cache shell

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE='ag-cute-blocks-v03';
+const CORE=['./','./index.html','./app.js','./manifest.webmanifest','https://cdn.jsdelivr.net/npm/three@0.180.0/build/three.module.js'];
+self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>Promise.allSettled(CORE.map(url=>cache.add(url)))));self.skipWaiting()});
+self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim()});
+self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;event.respondWith(caches.match(event.request).then(hit=>hit||fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy)).catch(()=>{});return response}).catch(()=>caches.match('./index.html'))))});
