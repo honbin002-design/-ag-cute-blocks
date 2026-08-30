5609894acfab578824265208484a31577fba4306
honbin002-design <honbin002@gmail.com>
2026-08-30 21:08:11 +0800

    chore: bump PWA cache for V0.4.1 controls and models

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='ag-cute-blocks-v04';
+const CACHE='ag-cute-blocks-v041';
 const CORE=['./','./index.html','./app-v04.js','./character-models.js','./animal-models.js','./crop-models.js','./economy-system.js','./farming-system.js','./manifest.webmanifest','https://cdn.jsdelivr.net/npm/three@0.180.0/build/three.module.js'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>Promise.allSettled(CORE.map(url=>cache.add(url)))));self.skipWaiting()});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim()});
